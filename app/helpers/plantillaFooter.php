02c5be96d301185dda9c138a39582b2a8b7f086f
SeishinF <fernatoolsgamer@gmail.com>
2021-04-14 00:50:24 -0600

    Creación de Index y Plantillas

---
+++ app/helpers/plantillaFooter.php
@@ -0,0 +1,17 @@
+</main>
+    
+    <footer>
+        <!-- Derechos Reservados -->
+        <div class="text-center p-3" id="ptDerechos">
+            @2021 Derechos Reservados:
+            <a class="text-white" href="http://www.megafrio.com/">MegaFrio </a>
+        </div>
+    </footer>
+
+
+
+<script src="../../resources/js/bootstrap/bootstrap.min.js"></script>
+<script src="https://kit.fontawesome.com/592eb2e9e3.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
