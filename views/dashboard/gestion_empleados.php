f6703fc66a34347ca77677912b3198efe0d1ba25
SeishinF <fernatoolsgamer@gmail.com>
2021-04-15 14:53:36 -0600

    Agregamos Gestiones

--- views/dashboard/gestion_empleados.php
+++ views/dashboard/gestion_empleados.php
@@ -5,15 +5,20 @@ include("../../app/helpers/plantillaHeader.php");
 
 <body>
 <br>
-  <center>
-    <h1 class="center">Gestión de empleados
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center" id="Titulo1">
+         <h1 class="center">Gestión de empleados
     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-file-earmark-person" viewBox="0 0 16 16">
       <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
       <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2v9.255S12 12 8 12s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h5.5v2z"/>
     </svg>
     </h1>
-    <center>
-    <hr>
+    
+      </div>
+    </div>
+  </div>
+
     <br><br>
     <div class="container">
       <div class="row">
