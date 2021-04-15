1f146cc920e10b9b08a2c0a564d6c757fe3024a7
SeishinF <fernatoolsgamer@gmail.com>
2021-04-14 18:47:05 -0600

    Finalizacion de Index

---
+++ views/dashboard/index.php
@@ -0,0 +1,139 @@
+<?php
+//Se incluye la plantilla del encabezado para la página web
+include("../../app/helpers/plantillaHeader.php");
+
+?>
+
+<Section>
+
+    <div class="container">
+        <br>
+        <div class="row">
+            
+            <div class="col-12 text-center" id="Titulo1">
+                <h1>Administracion</h1>
+                
+            </div>
+
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-12">
+                <!-- Agregamos los cuadritos -->
+                <div class="row">
+                    <!-- Proveedores -->
+                    <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-3 p-3 col-xxl-3 text-center">
+                        <a href="#">
+                        <div class="row">
+                            <div class="col-12" id="BoxSuperior">
+                                <img src="../../resources/img/materiales/proveedores.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-12" id="BoxInferior">
+                                <h4>Proveedores</h4>
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+                    <!-- Equipo -->
+                    <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-3 p-3 col-xxl-3 text-center">
+                        <a href="#">
+                        <div class="row">
+                            <div class="col-12" id="BoxSuperior">
+                                <img src="../../resources/img/materiales/proveedores.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-12" id="BoxInferior">
+                                <h4>Equipo</h4>
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+                    <!-- Clientes -->
+                    <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-3 p-3 col-xxl-3 text-center">
+                        <a href="#">
+                        <div class="row">
+                            <div class="col-12" id="BoxSuperior">
+                                <img src="../../resources/img/materiales/proveedores.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-12" id="BoxInferior">
+                                <h4>Clientes</h4>
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+                    <!-- Empleados -->
+                    <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-3 p-3 col-xxl-3 text-center">
+                        <a href="#">
+                        <div class="row">
+                            <div class="col-12" id="BoxSuperior">
+                                <img src="../../resources/img/materiales/proveedores.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-12" id="BoxInferior">
+                                <h4>Empleados</h4>
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+                    
+
+                </div>
+
+
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            
+            <div class="col-12 text-center" id="Titulo1">
+                <h1>Visualizar</h1>
+                
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <!-- Agregamos los cuadritos -->
+                <div class="row" >
+                    <!-- Agenda -->
+                    <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-3 p-3 col-xxl-6 text-center">
+                        <a href="#">
+                        <div class="row">
+                            <div class="col-12" id="BoxSuperior">
+                                <img src="../../resources/img/materiales/proveedores.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-12" id="BoxInferior">
+                                <h4>Agenda</h4>
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+                    <!-- Bitacora -->
+                    <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-3 p-3 col-xxl-6 text-center">
+                        <a href="#">
+                        <div class="row">
+                            <div class="col-12" id="BoxSuperior">
+                                <img src="../../resources/img/materiales/proveedores.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-12" id="BoxInferior">
+                                <h4>Bitacora</h4>
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <br>
+    </div>
+
+
+
+</Section>
+
+
+<?php
+//Se incluye la plantilla del encabezado para la página web
+include("../../app/helpers/plantillaFooter.php");
+?>
