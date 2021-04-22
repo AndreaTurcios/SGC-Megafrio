51c8f2d360c0c4c8800139c16a2150cdf194e3b9
Turcios Barquero Andrea María 1 <20190336@ricaldone.edu.sv>
2021-04-21 18:22:08 -0600

    cambios gestion empleados

--- views/dashboard/gestion_empleados.php
+++ views/dashboard/gestion_empleados.php
@@ -21,39 +21,43 @@ include("../../app/helpers/plantillaHeader.php");
     </div>
     <br>
     <div class="row">
+      <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 col-xxl-8 p-3">
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Buscar</button>
+        </form>
+      </div>
+      <!--Aquí en el botón de los filtros creamos un dropdown para poner las respectivas opciones -->
+      <div class="col-6 col-xs-6 col-sm-6 col-md-2 col-lg-2 col-xl-2 col-xxl-2 p-3 text-center" id="MuestraBTN">
+        <div class="dropdown">
+          <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            Filtros de búsqueda
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li><a class="dropdown-item" href="#">Representante</a></li>
+            <li><a class="dropdown-item" href="#">Compañía</a></li>
+            <li><a class="dropdown-item" href="#">Teléfono</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-6 col-xs-6 col-sm-6 col-md-2 col-lg-2 col-xl-2 col-xxl-2 p-3 text-center">
+        <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#ModalAgregarProveedor">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus"
+            viewBox="0 0 16 16">
+            <path
+              d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+          </svg>
+          Agregar
+        </button>
+      </div>
+    </div>
+    <div class="row">
       <nav class="navbar navbar-light bg-light">
         <div class="container-fluid">
-          <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 col-xxl-8 p-3">
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Buscar</button>
-            </form>
-          </div>
-          <!--Aquí en el botón de los filtros creamos un dropdown para poner las respectivas opciones -->
-          <div class="form-group">
-                          <div class="dropdown">
-                            <button class="btn dropdown-toggle" type="button" id="dropdownMenuLink3"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Filtros de búsqueda
-                            </button>
-                              <div class="dropdown-menu" aria-labelledby="dropdownMenuLink3">
-                                <a class="dropdown-item" href="#">Nombre</a>
-                                <a class="dropdown-item" href="#">Apellido</a>
-                                <a class="dropdown-item" href="#">Usuario</a>
-                                <a class="dropdown-item" href="#">Tipo empleado</a>
-                              </div>
-                            </div>
-                          </div>
+          
           <!--Aquí arrancamos con el botón agregar para abrir el modal -->
           <div class="col-6 col-xs-6 col-sm-6 col-md-2 col-lg-2 col-xl-2 col-xxl-2 p-3 text-center" id="MuestraBTN">
-            <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#ModalAgregarEmpleado">
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus"
-                viewBox="0 0 16 16">
-                <path
-                  d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-              </svg>
-              Agregar
-            </button>
             <!--Colocamos el div para el modal -->
             <div class="modal fade" id="ModalAgregarEmpleado" tabindex="-1" aria-labelledby="exampleModalLabel"
               aria-hidden="true">
@@ -132,6 +136,7 @@ include("../../app/helpers/plantillaHeader.php");
       <!--Colocamos la tabla -->
       <div class="row">
         <div class="col scroll">
+          <div class="table-responsive">
           <table class="table table-hover">
             <thead>
               <tr>
@@ -291,6 +296,7 @@ include("../../app/helpers/plantillaHeader.php");
               </tr>
             </tbody>
           </table>
+          </div>
           <!--Cerramos la tabla -->
         </div>
       </div>
