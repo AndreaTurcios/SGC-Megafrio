164c81550259da81212fff60f0fd34afc6907f14
FabioEstrada23 <65871577+FabioEstrada23@users.noreply.github.com>
2021-06-28 10:49:45 -0600

    Confirmación de estado

--- app/models/empleados.php
+++ app/models/empleados.php
@@ -189,11 +189,12 @@ class Empleados extends Validator{
 
     public function checkUser($nombreusuario)
     {
-        $sql = 'SELECT id_empleado, id_tipo_emp FROM empleado WHERE nombre_usuario = ?';
+        $sql = 'SELECT id_empleado, id_tipo_emp, estado FROM empleado WHERE nombre_usuario = ?';
         $params = array($nombreusuario);
         if ($data = Database::getRow($sql, $params)) {
             $this->id = $data['id_empleado'];
-            $this->nombre_usuario = $nombreusuario;
+            $this->idtipoempleado = $data['id_tipo_emp'];
+            $this->estado = $data['estado'];
             return true;
         } else {
             return false;
