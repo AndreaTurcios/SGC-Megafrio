29b01ca0f7e2a87c90cd860945a448b74226a485
FabioEstrada23 <65871577+FabioEstrada23@users.noreply.github.com>
2021-06-30 21:28:07 -0600

    Finalizacion de perfil

--- app/api/usuarios.php
+++ app/api/usuarios.php
@@ -13,7 +13,21 @@ if(isset($_GET['action'])) {
     $result = array('status' => 0, 'error' => 0, 'message' => null, 'exception' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
     if (isset($_SESSION['id_empleado'])) {
+
         switch ($_GET['action']) {
+            
+        case 'readProfile':
+                if ($result['dataset'] = $usuario->readProfile()) {
+                    $result['status'] = 1;
+                } else {
+                    if (Database::getException()) {
+                        $result['exception'] = Database::getException();
+                    } else {
+                        $result['exception'] = 'Usuario inexistente';
+                    }
+                }
+                break;    
+
         case 'logOut':
             if (session_destroy()) {
                 $result['status'] = 1;
@@ -22,6 +36,32 @@ if(isset($_GET['action'])) {
                 $result['exception'] = 'Ocurrió un problema al cerrar la sesión';
             }
             break;
+        case 'editProfile':  
+                $_POST = $usuario->validateForm($_POST);
+                if ($usuario->setNombreUsuario($_POST['username'])){
+                    if ($usuario->setNombreEmpleado($_POST['nombres'])) {
+                        if ($usuario->setApellidoEmpleado($_POST['apellidos'])) {
+                            if ($usuario->setTelefonoEmpleado($_POST['telefono'])) {
+                                if ($usuario->editProfile()) {
+                                    $result['status'] = 1;
+                                    $_SESSION['nombre_usuario'] = $usuario->getNombreUsuario();
+                                    $result['message'] = 'Perfil modificado correctamente';
+                                } else {
+                                    $result['exception'] = Database::getException();
+                                }
+                            } else{
+                                $result['exception'] = 'Télefono incorrecto';
+                            }  
+                        } else{
+                            $result['exception'] = 'Apellidos de empleado incorrectos';
+                        }  
+                    } else{
+                        $result['exception'] = 'Nombres de empleado incorrectos';
+                    }   
+                } else{
+                    $result['exception'] = 'Nombre de usuario incorrecto';
+                }     
+                break;    
             default:
                 $result['exception'] = 'Acción no disponible dentro de la sesión';
         }    
