f4ccd802d336a9d0d1520dae3ab238b1c74dbed4
Steven2245 <stevenalfonso225@gmail.com>
2021-06-30 18:28:26 -0600

    pais final

---
+++ app/api/pais.php
@@ -0,0 +1,132 @@
+<?php
+require_once('../helpers/database.php');
+require_once('../helpers/validator.php');
+require_once('../models/pais.php');
+
+if (isset($_GET['action'])) {
+    // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
+    session_start();
+    // Se instancia la clase correspondiente.
+    $pais = new Pais;
+    // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+    $result = array('status' => 0, 'message' => null, 'exception' => null);
+    // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
+        // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'readAll':
+                if ($result['dataset'] = $pais->readAll()) {
+                    $result['status'] = 1;
+                } else {
+                    if (Database::getException()) {
+                        $result['exception'] = Database::getException();
+                    } else {
+                        $result['exception'] = 'No hay ningún pais ingresado en la base de datos';
+                    }
+                }
+                break;
+                case 'search':
+                    $_POST = $pais->validateForm($_POST);
+                    if ($_POST['search'] != '') {
+                        if ($result['dataset'] = $pais->searchRows($_POST['search'])) {
+                            $result['status'] = 1;
+                            $rows = count($result['dataset']);
+                            if ($rows > 1) {
+                                $result['message'] = 'Se encontraron ' . $rows . ' coincidencias';
+                            } else {
+                                $result['message'] = 'Solo existe una coincidencia';
+                            }
+                        } else {
+                            if (Database::getException()) {
+                                $result['exception'] = Database::getException();
+                            } else {
+                                $result['exception'] = 'No hay coincidencias';
+                            }
+                        }
+                    } else {
+                        $result['exception'] = 'Ingrese un valor para buscar';
+                    }
+                    break;
+            case 'readOne':
+                if ($pais->setId($_POST['id_pais'])) {
+                    if ($result['dataset'] = $pais->readOne()) {
+                        $result['status'] = 1;
+                    } else {
+                        if (Database::getException()) {
+                            $result['exception'] = Database::getException();
+                        } else {
+                            $result['exception'] = 'No existe el respectivo pais';
+                        }
+                    }
+                } else {
+                    $result['exception'] = 'Pais incorrecto';
+                }
+                break;
+
+                case 'create':
+                    $_POST = $pais->validateForm($_POST);
+                    if($pais->setNombreP($_POST['nombre_pais'])){
+                        if($pais->setCodigo($_POST['codigo_postal'])){
+                            if ($pais->createRow()) {
+                                $result['status'] = 1;
+                                $result['message'] = 'Pais creado correctamente';
+                                
+                            } else {
+                                $result['exception'] = Database::getException();
+                            }
+                        }else{
+                            $result['message'] = 'Pais Incorrecto';
+                        }   
+                    }else{
+                        $result['message'] = 'Codigo postal Incorrecto';
+                    }
+                    break;
+
+                    case 'update':
+                        $_POST = $pais->validateForm($_POST);
+                        if ($pais->setId($_POST['id_pais'])) {
+                            if ($data = $pais->readOne()) {
+                                if($pais->setNombreP($_POST['categoria2'])){
+                                    if($pais->setCodigo($_POST['categoria2'])){
+                                        if ($pais->updateRow()) {
+                                            $result['status'] = 1;
+                                            $result['message'] = 'Pais actualizadao correctamente';
+                                        } else {
+                                            $result['exception'] = Database::getException();
+                                        }
+                                    }else{
+                                        $result['message'] = 'Codigo postal incorrecto';
+                                    }   
+                                }else{
+                                    $result['message'] = 'Nombre de pais incorrecto';
+                                }
+                            }
+                        }   
+                        break;
+
+            case 'delete':
+                if ($pais->setId($_POST['id_pais'])) {
+                    if ($data = $pais->readOne()) {
+                        if ($pais->deleteRow()) {
+                            $result['status'] = 1;
+                            $result['message'] = 'Pais eliminado correctamente'; 
+                           
+                        } else {
+                            $result['exception'] = Database::getException();
+                        }
+                    } else {
+                        $result['exception'] = 'Pais inexistente';
+                    }
+                } else {
+                    $result['exception'] = 'Pais incorrecto';
+                }
+                break;               
+            default:
+                $result['exception'] = 'Acción no disponible dentro de la sesión';
+        }
+        // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+        header('content-type: application/json; charset=utf-8');
+        // Se imprime el resultado en formato JSON y se retorna al controlador.
+        print(json_encode($result));
+} else {
+    print(json_encode('Recurso no disponible'));
+}
---
+++ app/controllers/pais.js
@@ -0,0 +1,47 @@
+// Constante para establecer la ruta y parámetros de comunicación con la API.
+const API_PAIS = '../../app/api/pais.php?action=';
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Se llama a la función que obtiene los registros para llenar la tabla. Se encuentra en el archivo components.js
+    readRows(API_PAIS);
+});
+
+// Función para llenar la tabla con los datos de los registros. Se manda a llamar en la función readRows().
+function fillTable(dataset) {
+    let content = '';
+    // Se recorre el conjunto de registros (dataset) fila por fila a través del objeto row.
+    dataset.map(function (row) {       
+        // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+        content += `
+        <tr>
+        <td>${row.id_pais}</td>            
+        <td>${row.nombre_pais}</td>
+        <td>${row.codigo_postal}</td>   
+        <td>
+            <a href="#" onclick="openUpdateDialog(${row.id_pais})"class="btn"  data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</a> /
+            <a href="#" onclick="openDeleteDialog(${row.id_pais})"class="btn">Eliminar</a>
+        </td>
+    </tr>
+        `;
+    });
+    // Se agregan las filas al cuerpo de la tabla mediante su id para mostrar los registros.
+    document.getElementById('tbody-rows').innerHTML = content;
+    //M.Tooltip.init(document.querySelectorAll('.tooltipped'));
+}
+// Método manejador de eventos que se ejecuta cuando se envía el formulario de buscar.
+document.getElementById('search-form').addEventListener('submit', function (event) {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se llama a la función que realiza la búsqueda. Se encuentra en el archivo components.js
+    searchRows(API_PAIS, 'search-form');
+});
+
+// Método manejador de eventos que se ejecuta cuando se envía el formulario de guardar.
+document.getElementById('save-form').addEventListener('submit', function (event) {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    
+    saveRow(API_PAIS, 'create', 'save-form', null);
+    document.getElementById('save-form').reset();
+});
+
