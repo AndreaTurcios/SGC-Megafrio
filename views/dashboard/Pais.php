2229d59631b67bcf95f6c3e6415fab70aaa0839b
Steven2245 <stevenalfonso225@gmail.com>
2021-07-02 15:40:14 -0600

    Update Pais.php

--- views/dashboard/Pais.php
+++ views/dashboard/Pais.php
@@ -54,10 +54,71 @@ plantillaHeader::headerTemplate('Gestión País');
     </div>
     </nav>
                     
-          <div class="mx-auto" class="col-sm-12 col-md-12 col-lg-12 col-xl-8 offset-xl-1">
-            <div class="row">
-           </div>
-          </nav>
+                    <!-- Creacion del buscador -->
+                    <div class="mx-auto" class="col-sm-12 col-md-12 col-lg-12 col-xl-8 offset-xl-1">
+                      <div class="row">
+                    
+                          <div class="container-fluid">
+                    
+
+                    <div class="col-6 col-xs-6 col-sm-6 col-md-2 col-lg-2 col-xl-2 col-xxl-2 p-3 text-center" id="MuestraBTN">
+                    
+                    <div id="ModalAgregarPais" class="modal fade">
+            <div class="container-fluid">
+            <form method="post" id="save-form">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+
+                  <h5 class="modal-title" id="exampleModalLabel">Agregar pais</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <!--Aquí comenzamos con el body del modal -->
+                <div class="modal-body">
+                  <div class="container">
+                    <div class="row">
+                      <form>
+                        <div class="form-group ">
+                          <label for="nombre_pais">Nombre pais:</label>
+                          <input type="text" class="form-control" id="nombre_pais" name="nombre_pais" placeholder="Pais">
+                        </div>
+                        <div class="form-group">
+                          <label for="codigo_postal">Codigo postal:</label>
+                          <input type="text" class="form-control" id="codigo_postal" name="codigo_postal" placeholder="codigo postal">
+                        </div>
+                      <br>
+
+                <!--Aquí arrancamos con el footer del modal -->
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                  <button type ="submit" class="btn waves-effect blue tooltipped"data-tooltip="Guardar" >Guardar</button><br>
+                  <hr>
+                 <br>
+                  <hr>
+                  
+                  </div>
+                    </div>
+                </div>
+                </div>
+                </form>
+                  </div>
+                </div>
+              </div>
+              </div>
+            </div>
+
+          
+
+                    
+
+
+                    
+                        
+                    </div>
+
+                </div>
+            </nav>
 
             <!-- Creacion de la tabla -->
                       </div>
@@ -81,8 +142,7 @@ plantillaHeader::headerTemplate('Gestión País');
                     </div>
                   </div>
                 </div>
-                </div>
-            </nav>
+                
                 </section>
 
 <?php
