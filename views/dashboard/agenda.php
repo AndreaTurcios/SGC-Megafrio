7a2b67cdc64ace3d5ef1a1ab1a8b75cce81e494f
FabioEstrada23 <65871577+FabioEstrada23@users.noreply.github.com>
2021-07-04 10:33:05 -0600

    Comienzo de agenda

--- views/dashboard/agenda.php
+++ views/dashboard/agenda.php
@@ -1,121 +1,149 @@
 <?php
-//Se incluye la clase con las plantillas del documento
-include("../../app/helpers/plantillaHeader.php");
-//Se imprime la plantilla del encabezado y se envía el titulo para la página web
-plantillaHeader::headerTemplate('Agenda');
+// Se incluye la clase con las plantillas del documento.
+require_once('../../app/helpers/dashboard_page.php');
+// Se imprime la plantilla del encabezado enviando el título de la página web.
+Dashboard_Page::headerTemplate('Agenda');
 ?>
 
 <br>
-<div class="container">
-    <div class="row">
-        <div class="col-12 text-center" id="Titulo1">
-            <h1>Vista de Agenda</h1>
-        </div>
-    </div>
-</div>
+<section>
 
+            <div class="container">
+                <div class="row">
 
-<div id="container">
-
-    <div class="box">
-        <div id="header">
-            <div id="monthDisplay"></div>
-            <div>
-                <button id="backButton">Atrás</button>
-                <button id="nextButton">Siguiente</button>
+                    <div class="modal fade" id="ModalCreate" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modal-title">Actualizar Empleados</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form id="update-form" method="post" enctype="multipart/form-data">
+                                <div class="form-group d-none">
+                                    <label for="formGroupExampleInput">ID:</label>
+                                    <input type="text" class="form-control " placeholder="Ej: MalteHC..."
+                                        aria-label="Buscar" aria-describedby="basic-addon1" id="id_empleado" type="text"
+                                        name="id_empleado" class="validate" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput">Nombres:</label>
+                                    <input type="text" class="form-control" placeholder="Ej: MalteHC..."
+                                        aria-label="Buscar" aria-describedby="basic-addon1" id="nombres_emp2" type="text"
+                                        name="nombres_emp2" class="validate" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput2">Apellidos:</label>
+                                    <input type="text" class="form-control"
+                                        placeholder="Ej: MalteHC..." aria-label="Buscar"
+                                        aria-describedby="basic-addon1" id="apellidos_emp2" type="text"
+                                        name="apellidos_emp2" class="validate" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput2">correo:</label>
+                                    <input type="text" class="form-control"
+                                        placeholder="Ej: fulanito@gmail.com..." aria-label="Buscar"
+                                        aria-describedby="basic-addon1" id="correo_emp2" type="text"
+                                        name="correo_emp2" class="validate" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput3">Alias:</label>
+                                    <input type="text" class="form-control" placeholder="Ej: MalteHC..."
+                                        aria-label="Buscar" aria-describedby="basic-addon1" id="alias_emp2" type="text"
+                                        name="alias_emp2" class="validate" required>
+                                </div>
+                                
+                                <div class="form-group">
+                                    <label for="tipo_empleado2">Tipo empleado:</label>
+                                    <select class="form-select" aria-label="Select" id="tipo_empleado2" name="tipo_empleado2">
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="estado_emp2">estado empleado:</label>
+                                    <select class="form-select" aria-label="Select" id="estado_emp2" name="estado_emp2">
+                                    </select>
+                                </div>
+                        </div>
+                        
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                            <button type="submit" data-tooltip="Actualizar" class="btn btn-primary" >Guardar Cambios</button>
+                        </div>
+                    </form>
+                    </div>
+                </div>
             </div>
-        </div>
 
-        <div id="weekdays">
-            <div>Domingo</div>
-            <div>Lunes</div>
-            <div>Martes</div>
-            <div>Miércoles</div>
-            <div>Jueves</div>
-            <div>Viernes</div>
-            <div>Sábado</div>
-        </div>
 
-        <div id="calendar"></div>
-    </div>
 
-    <div id="newEventModal">
-        <h2>Nuevo evento</h2>
+            
+                    <div class="col-12 text-center">
+            
+                        <div class="row">
+                            <div class="col-12 text-center" id="Titulo1">
+                                <h1>Agenda para este dia</h1>
+                            </div>
+                            <nav class="navbar navbar-light bg-light">
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 col-xxl-8 p-3">
+                                    <form class="d-flex" method="post" id="search-form">
+                                        <input id = "search" placeholder="Ingrese el nombre del cliente" class="form-control me-2" type="text" name ="search" required/>
+                                        <input type="date" class="form-control" placeholder="YYYY-MM-DD"
+                                            aria-describedby="basic-addon1" id="fecha_nac_cli"
+                                            name="fecha_nac_cli" class="validate" required>
+                                        <button class="btn btn-outline-success" type="submit">Buscar</button>
+                                    </form>
+                                    </div>
+                                    
+                                
+                                    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#Modalcreate">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus"
+                                        viewBox="0 0 16 16">
+                                        <path
+                                        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                                    </svg>
+                                    Agregar
+                                    </button>
+                                
+                                </div>
+                            </nav>
+            
+                            <div class="col-12 p- text-center">
+                                <div class="table-responsive">
+                                    <table class="table table-dark table-striped" id="tabla_emp">
+                                        <thead>
+            
+                                            <tr>
+                                                <th scope="col">Nombre</th>
+                                                <th scope="col">Apellidos</th>
+                                                <th scope="col">Correo</th>
+                                                <th scope="col">Alias</th>
+                                                <th scope="col">Tipo emp</th>
+                                                <th scope="col">Estado_emp</th>
+                                                <th class="actions-column">Acciones</th>
+                                            </tr>
+                                        </thead>
 
-        <input id="eventTitleInput" placeholder="Tarea" />
+                                        <tbody id="tbody-rows">
+                                        </tbody>
+                                    </table>
+                                </div>
+            
+            
+                            </div>
+                        </div>
+            
+                    </div>
+                </div>
+            </div>
+            </section>
 
-        <select id="est-select">
-            <option disabled selected>Seleccionar un estado</option>
-            <option value="1">En proceso</option>
-            <option value="2">Finalizada</option>
-        </select>
 
-        <select id="cli-select">
-            <option disabled selected>Seleccionar un cliente</option>
-            <option value="1">Zachery C. Horton</option>
-            <option value="2">Naomi J. Fitzpatrick</option>
-            <option value="3">Mariko Z. Casey</option>
-            <option value="4">Timon K. Macias</option>
-        </select>
 
-        <select id="emp-select">
-            <option disabled selected>Seleccionar un empleado</option>
-            <option value="1">Gregory H. Maxwell</option>
-            <option value="2">Ramona I. Woodk</option>
-            <option value="3">Teagan I. Grimes</option>
-            <option value="4">Duncan X. Fitzgerald</option>
-            <option value="5">Deborah S. Pearson</option>
-        </select>
-
-        <select id="ser-select">
-            <option disabled selected>Seleccionar un tipo de servicio</option>
-            <option value="1">Mantenimiento</option>
-            <option value="2">Instalacion</option>
-            <option value="3">Revision</option>
-            <option value="4">Verificacion de zonas</option>
-            <option value="5">Atencion al cliente</option>
-        </select>
-
-        <form name="formulario" method="post" action="/send.php">
-            <!-- Campo de entrada de fecha -->
-            Selecciona la fecha de programación:
-            <input type="date" name="fecha" min="2000-01-01" max="2023-12-31" step="2" />
-            <br>
-            <!-- Campo de entrada de hora -->
-            Selecciona la hora de programación:
-            <input type="time" name="hora" min="18:00" max="21:00" step="3600" />
-
-            <!-- Campo de entrada de fecha provisional-->
-            Selecciona la fecha provisional:
-            <input type="date" name="fecha-pro" min="2000-01-01" max="2023-12-31" step="2" />
-            <br>
-            <!-- Campo de entrada de provisional -->
-            Selecciona la hora provisional:
-            <input type="time" name="hora-pro" min="18:00" max="21:00" step="3600" />
-        </form>
-
-        <br>
-
-        <button id="saveButton">Guardar</button>
-        <button id="cancelButton">Cancelar</button>
-    </div>
-
-    <div id="deleteEventModal">
-        <h2>Tarea</h2>
-
-        <p id="eventText"></p>
-
-        <button id="editButton">Modificar</button>
-        <button id="deleteButton">Borrar</button>
-        <button id="closeButton">Cerrar</button>
-    </div>
-</div>
-
-<div id="modalBackDrop"></div>
 
 
 
 <?php
-//Se incluye la plantilla del encabezado para la página web
-include("../../app/helpers/plantillaFooter.php");
+// Se imprime la plantilla del encabezado enviando el título de la página web.
+Dashboard_Page::footerTemplate('agenda.js');
 ?>
