270defaa9e5ec0a3d1df78296381c477855af1b6
AndreaTurcios <20190336@ricaldone.edu.sv>
2021-07-09 08:37:23 -0600

    prueba

--- app/controllers/empleados.js
+++ app/controllers/empleados.js
@@ -105,7 +105,7 @@ function openUpdateDialog(id) {
     }
 });
 
-document.getElementById('save-form').addEventListener('submit', function (event) {
+document.getElementById('save-form').addEventListener('submit', function (event) {  
     // Se evita recargar la página web después de enviar el formulario.
     event.preventDefault();
     // Se define una variable para establecer la acción a realizar en la API.
