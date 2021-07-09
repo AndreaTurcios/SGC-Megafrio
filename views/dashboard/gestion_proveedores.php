ac05071a099c7e45fbb9bd48bec002b89482c441
AndreaTurcios <mturcios@nexuserp.com>
2021-07-09 13:29:39 -0600

    :V

--- views/dashboard/gestion_proveedores.php
+++ views/dashboard/gestion_proveedores.php
@@ -138,7 +138,7 @@ Dashboard_Page::headerTemplate('Proveedores');
       <div class="modal-content">
         <div class="modal-header">
 
-          <h5 class="modal-title" id="exampleModalLabel">Actualizar proveedores22</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Actualizar proveedores</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
 
