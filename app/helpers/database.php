39617e6f8b1ece94ad4004980b0c13a86ca585a8
FabioEstrada23 <65871577+FabioEstrada23@users.noreply.github.com>
2021-07-15 20:18:26 -0600

    More images

--- app/helpers/database.php
+++ app/helpers/database.php
@@ -16,9 +16,9 @@ class Database
     {
         // Credenciales para establecer la conexión con la base de datos.
         $server = 'localhost';
-        $database = 'BaseMegafrio';   
+        $database = 'MEGAFRIO';   
         $username = 'postgres';
-        $password = 'admin';
+        $password = '02032003';
    
         // Se crea la conexión mediante la extensión PDO y el controlador para PostgreSQL.
         self::$connection = new PDO('pgsql:host='.$server.';dbname='.$database.';port=5432', $username, $password);
