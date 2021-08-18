c4d88005f244451e10f1c52a11ea745419b72a79
AndreaTurcios <20190336@ricaldone.edu.sv>
2021-08-18 14:25:27 -0600

    pruebas xd

--- app/reports/empleadosg.php
+++ app/reports/empleadosg.php
@@ -1,14 +1,14 @@
 <?php
 require('../../app/helpers/report.php');
-require('../../app/models/empleados.php');
+require('../../app/models/tipo_empleado.php');
 // Se instancia la clase para crear el reporte.
 $pdf = new Report;
 // Se inicia el reporte con el encabezado del documento.
-$pdf->startReport('Reporte de datos de empleado');
+$pdf->startReport('Reporte de datos de empleado por tipo empleado');
 // Se instancia el módelo Categorías para obtener los datos.
-$empleados = new Empleados;
+$templeados = new tipoEmpleado;
 // Se verifica si existen registros (categorías) para mostrar, de lo contrario se imprime un mensaje.
-if ($dataEmpleados = $empleados->readAll()) {
+if ($dataEmpleados = $templeados->readAll()) {
     // Se recorren los registros ($dataCategorias) fila por fila ($rowCategoria).
     foreach ($dataEmpleados as $rowEmpleados) {
         $pdf->SetFont('Arial', '', 11);
@@ -16,13 +16,13 @@ if ($dataEmpleados = $empleados->readAll()) {
         // Se imprime una celda con el nombre de la categoría.
         $pdf->Cell(193, 10, utf8_decode('Tipo empleado: '.$rowEmpleados['tipoemp']), 1, 1, 'C', 1);
         // Se establece la categoría para obtener sus productos, de lo contrario se imprime un mensaje de error.
-        if ($empleados->setId($rowEmpleados['id_empleado'])) {
+        if ($templeados->setId($rowEmpleados['id_tipo_emp'])) {
             // Se verifica si existen registros (productos) para mostrar, de lo contrario se imprime un mensaje.
-            if ($dataEmpleados = $empleados->readReport()) {
+            if ($dataEmpleados = $templeados->readEmpleado()) {
                 // Se establece un color de relleno para los encabezados.
                 $pdf->SetFillColor(225);
                 // Se establece la fuente para los encabezados.
-                $pdf->SetFont('Times', 'B', 11);
+                $pdf->SetFont('Arial', '', 11);
                 // Se imprimen las celdas con los encabezados.
                 $pdf->Cell(60, 10, utf8_decode('Nombres'), 1, 0, 'C', 1);
                 // Se establece la fuente para los datos de los productos.
@@ -30,9 +30,10 @@ if ($dataEmpleados = $empleados->readAll()) {
                 $pdf->Cell(33, 10, utf8_decode('Usuario'), 1, 0, 'C', 1);
                 $pdf->Cell(40, 10, utf8_decode('Teléfono'), 1, 0, 'C', 1);
                 $pdf->SetFont('Arial', '', 11);
+                $pdf->Ln();
                 // Se recorren los registros ($dataProductos) fila por fila ($rowProducto).
                 foreach ($dataEmpleados as $rowEmpleados) {
-                    $pdf->Ln();
+                    
                     $pdf->Cell(60, 10, utf8_decode($rowEmpleados['nombre_emp']), 1, 0);
                     $pdf->Cell(60, 10, utf8_decode($rowEmpleados['apellido_emp']), 1, 0);
                     $pdf->Cell(33, 10, utf8_decode($rowEmpleados['nombre_usuario']), 1, 0);
