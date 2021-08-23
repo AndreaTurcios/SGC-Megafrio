1c4d9c8826028769291c385605b013888a8d80ca
AndreaTurcios <20190336@ricaldone.edu.sv>
2021-08-22 23:42:28 -0600

    prueba x100 de cuenta de git xd

--- views/dashboard/gestion_proveedores.php
+++ views/dashboard/gestion_proveedores.php
@@ -32,7 +32,7 @@ Dashboard_Page::headerTemplate('Proveedores');
           <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#ModalAgregarProveedor">
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus"
               viewBox="0 0 16 16">
-              <path
+              <path   
                 d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
             </svg>  
             Agregar     
